677abad9ec96395f4fb86011db79d6ae561b1347
Jeremy Liss <jeremyliss@gmail.com>
2017-03-25 17:08:54 +1300

    style with css

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 var request = require('superagent')
 var React = require('react')
 var ReactDOM = require('react-dom')
-var hrefHandler = require('sheet-router/href')
 
 var state = require('./state')
-
-hrefHandler((click) => {
-  state.path = click.pathname
-  render(state)
-})
 
 state.path = '/'
 
@@ -34,7 +28,7 @@ if(mm<10){
 var today = yyyy+'-'+mm+'-'+dd;
 
 request
-  .get('https://api.nasa.gov/neo/rest/v1/feed?start_date='+today+'&end_date='+today+'&api_key=[insert api key]')
+  .get('https://api.nasa.gov/neo/rest/v1/feed?start_date='+today+'&end_date='+today+'&api_key=[inset api key])
   .end(function(err, res){
     if (err) {
       throw err
@@ -70,8 +64,8 @@ function getAsteroids(asteroids) {
 function AsteroidInfo () {
   return (
     <div>
-      {hazardous > 0 ? <h1>Yes: {hazardous}!</h1> : <h1>No!</h1>}
-      <p>According to NASA {asteroidCount} {roids} {isAre} approaching Earth today and {hazardous} {hazIsAre} potentially hazardous</p>
+      {hazardous > 0 ? <h1>Yes, {hazardous}!</h1> : <h1>No, not today!</h1>}
+      <p>According to NASA {asteroidCount} {roids} {isAre} approaching Earth today, {hazardous} of which {hazIsAre} potentially hazardous</p>
     </div>
   )
 }
@@ -83,9 +77,10 @@ function showInfo() {
 
 function App () {
   return(
-    <div>
+    <div id='App'>
       <h1><a href='#' onClick={showInfo}>Are There any Potentially Hazardous Asteroids Approaching Earth Today?</a></h1>
-      {state.display ? <AsteroidInfo /> : ''}
+      {state.display && <AsteroidInfo /> }
+      <img src="http://space-facts.com/wp-content/uploads/asteroids.png" />
     </div>
   )
 }
