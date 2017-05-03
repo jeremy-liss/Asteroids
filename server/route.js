c9e01d3ef2bd8635a4f17e9c182b74fe97f9bf67
Jeremy Liss <jeremyliss@gmail.com>
2017-05-03 14:44:46 +1200

    refactor route

--- server/route.js
+++ server/route.js
@@ -5,21 +5,10 @@ var env = [process.env.NODE_ENV || 'development']
 var request = require ('superagent')
 require('dotenv').config()
 
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1;
+var date = require('../lib/getToday')
 
-var yyyy = today.getFullYear();
-if(dd<10){
-    dd='0'+dd;
-}
-if(mm<10){
-    mm='0'+mm;
-}
-
-var today = yyyy+'-'+mm+'-'+dd;
+var today = date()
 var URL = 'https://api.nasa.gov/neo/rest/v1/feed?start_date='+today+'&end_date='+today+'&api_key='+process.env.KEY
-
 
 request
   .get(URL)
