3b1f22c4cb40e9ebe380ec203754efd731b8cc94
Jeremy Liss <jeremyliss@gmail.com>
2017-05-17 16:11:46 +1200

    seperate getAsteroids function

--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ var ReactDOM = require('react-dom')
 
 var state = require('./state')
 var date = require('../lib/getDate')
+var getAsteroids = require('../lib/getAsteroids')
 var App = require('./App')
 
 var today = date(0)
@@ -14,32 +15,11 @@ request
     if (err) {
       throw err
     } else {
-        asteroids = res.body.near_earth_objects[today]
-        getAsteroids(asteroids)
-        if (asteroidCount == 1) {
-          state.roids = 'asteroid'
-          state.isAre = 'is'
-        } else {
-          state.roids = 'asteroids'
-          state.isAre = 'are'
-        }
+        state.asteroids = res.body.near_earth_objects[today]
+        getAsteroids(state.asteroids)
         render ()
     }
-  });
-
-function getAsteroids(asteroids) {
-  asteroids.map(function(asteroid) {
-    state.asteroidCount +=1
-    if (asteroid.is_potentially_hazardous_asteroid){
-      state.hazardous += 1
-    }
   })
-  if (state.hazardous == 1){
-    state.hazIsAre = 'is'
-  } else {
-    state.hazIsAre = 'are'
-  }
-}
 
 function showInfo() {
   state.display = !state.display
